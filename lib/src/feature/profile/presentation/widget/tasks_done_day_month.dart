0c04ecd68e7f3f13e14c9f94a15dc246e781f482
ahmedfayez90 <122109723+ahmedfayez90@users.noreply.github.com>
2024-02-14 22:24:03 +0200

    complete profile logic

--- lib/src/feature/profile/presentation/widget/tasks_done_day_month.dart
+++ lib/src/feature/profile/presentation/widget/tasks_done_day_month.dart
@@ -5,78 +5,87 @@ class TasksDoneDayAndMonth extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-      height: 203.h,
-      width: 381.w,
-      child: Card(
-        elevation: 5,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusDirectional.circular(12.r),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(15.w),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: 160.w),
-                child: Column(
-                  children: [
-                    20.isHeight,
-                    const Expanded(
-                      child: AnimatedCircularProgressIndicator(
-                        percentage: 30 / 100,
-                      ),
-                    ),
-                    20.isHeight,
-                    Column(
+    return BlocBuilder<ProfileCubit, ProfileState>(
+      builder: (context, state) {
+        var cubit = context.read<ProfileCubit>();
+
+        return SizedBox(
+          height: 203.h,
+          width: 381.w,
+          child: Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusDirectional.circular(12.r),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(15.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 160.w),
+                    child: Column(
                       children: [
-                        Text(
-                          AppStrings.tasksCompletedForTheDay.tr(context),
-                          overflow: TextOverflow.ellipsis,
-                          style: FontStyles.interSize13_400Black,
+                        20.isHeight,
+                        Expanded(
+                          child: AnimatedCircularProgressIndicator(
+                            percentage:
+                                (cubit.profileModel?.missionsPerToday ?? 0.0) /
+                                    100,
+                          ),
                         ),
-                        Text(
-                          "${AppStrings.number.tr(context)} = 4",
-                          style: FontStyles.interSize14_800Primary,
+                        20.isHeight,
+                        Column(
+                          children: [
+                            Text(
+                              AppStrings.tasksCompletedForTheDay.tr(context),
+                              overflow: TextOverflow.ellipsis,
+                              style: FontStyles.interSize13_400Black,
+                            ),
+                            Text(
+                              "${AppStrings.number.tr(context)} = ${cubit.profileModel?.missionsPassedToday ?? 0.0}",
+                              style: FontStyles.interSize14_800Primary,
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                ),
-              ),
-              ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: 160.w),
-                child: Column(
-                  children: [
-                    20.isHeight,
-                    const Expanded(
-                      child: AnimatedCircularProgressIndicator(
-                        percentage: 74 / 100,
-                      ),
                     ),
-                    20.isHeight,
-                    Column(
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 160.w),
+                    child: Column(
                       children: [
-                        Text(
-                          AppStrings.tasksCompletedForThisMonth
-                              .tr(context),
-                          overflow: TextOverflow.ellipsis,
-                          style: FontStyles.interSize13_400Black,
+                        20.isHeight,
+                        Expanded(
+                          child: AnimatedCircularProgressIndicator(
+                            percentage:
+                                (cubit.profileModel?.missionsPerMonth ?? 0.0) /
+                                    100,
+                          ),
                         ),
-                        Text(
-                          "${AppStrings.number.tr(context)} = 44",
-                          style: FontStyles.interSize14_800Primary,
+                        20.isHeight,
+                        Column(
+                          children: [
+                            Text(
+                              AppStrings.tasksCompletedForThisMonth.tr(context),
+                              overflow: TextOverflow.ellipsis,
+                              style: FontStyles.interSize13_400Black,
+                            ),
+                            Text(
+                              "${AppStrings.number.tr(context)} = ${cubit.profileModel?.missionsPassedMonth ?? 0.0}",
+                              style: FontStyles.interSize14_800Primary,
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
