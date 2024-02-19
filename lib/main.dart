6b50faeb0c1b9b919f967d164c37323369a8242e
ahmedfayez90 <122109723+ahmedfayez90@users.noreply.github.com>
2024-02-19 15:01:05 +0200

    config notification 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,37 @@
 
 import 'package:ct_clean/src/core/config/routes/app_imports.dart';
+import 'package:ct_clean/src/core/services/background_service.dart';
+import 'dart:async';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Permission.notification.isDenied.then((value) {
+    if(value){
+      Permission.notification.request();
+    }
+  });
   await setUpLocators();
   await CacheHelper.initCacheHelper();
   print(UserLocal.lang);
    Bloc.observer = MyBlocObserver();
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings androidInitializationSettings =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: androidInitializationSettings,
+  );
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
+  await initializeServices();
+  FlutterBackgroundService().invoke("setAsBackground");
   runApp(  MyApp());
 }
