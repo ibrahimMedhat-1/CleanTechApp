ce1683a9c0a2093fff69b0bba2dc20f412b45a27
ahmedfayez90 <122109723+ahmedfayez90@users.noreply.github.com>
2024-03-06 22:30:36 +0200

    add missionId in prams sendStreamPosition

--- lib/src/feature/task_details/logic/models/stream_position_params.dart
+++ lib/src/feature/task_details/logic/models/stream_position_params.dart
@@ -3,25 +3,26 @@ import 'package:ct_clean/src/core/config/routes/app_imports.dart';
 class StreamPositionParams {
   final double lat;
   final double lng;
+  final int missionId;
 
-  StreamPositionParams({
-    this.lat = 0.0,
-    this.lng = 0.0,
-  });
+  StreamPositionParams({this.lat = 0.0, this.lng = 0.0, this.missionId = 0});
 
   StreamPositionParams copyWith({
     double? lat,
     double? lng,
+    int? missionId,
   }) {
     return StreamPositionParams(
       lat: lat ?? this.lat,
       lng: lng ?? this.lng,
+      missionId: missionId ?? this.missionId,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'driverId': UserLocal.driverId,
+      "missionId": missionId,
       'lat': lat,
       'lng': lng,
     };
